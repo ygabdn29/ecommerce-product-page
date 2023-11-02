339501bbf64c84e1eacc3d4a964836ec2dd357a8
Yoga <yogabudiman07@gmail.com>
2023-11-02 22:52:32 +0700

    Desktop cart container

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const mobileNavOpen = document.querySelector(".header__menu-toggle");
+const mobileNavClose = document.querySelector(".header__menu-close");
+const mobileMenu = document.querySelector(".header__navigation");
+const pageOverlay = document.querySelector(".overlay");
+
+mobileNavOpen.addEventListener("click", function () {
+  mobileMenu.classList.toggle("header__navigation--active");
+  pageOverlay.classList.toggle("overlay--active");
+  mobileNavClose.classList.remove("header__menu-close--hidden");
+});
+
+mobileNavClose.addEventListener("click", function () {
+  mobileMenu.classList.toggle("header__navigation--active");
+  pageOverlay.classList.toggle("overlay--active");
+  mobileNavClose.classList.add("header__menu-close--hidden");
+});
