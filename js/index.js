2f32e0b63b7cca9da2cf56a2cd59b5542f6b447a
Yoga <yogabudiman07@gmail.com>
2023-11-04 00:54:48 +0700

    Desktop and mobile general layout

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const mobileNavOpen = document.querySelector(".header__menu-toggle");
 const mobileNavClose = document.querySelector(".header__menu-close");
 const mobileMenu = document.querySelector(".header__navigation");
 const pageOverlay = document.querySelector(".overlay");
+const cartOpen = document.querySelector(".header__cart-icon");
+const cartDetail = document.querySelector(".header__cart-detail");
 
 mobileNavOpen.addEventListener("click", function () {
   mobileMenu.classList.toggle("header__navigation--active");
@@ -15,4 +17,8 @@ mobileNavClose.addEventListener("click", function () {
   mobileMenu.classList.toggle("header__navigation--active");
   pageOverlay.classList.toggle("overlay--active");
   mobileNavClose.classList.add("header__menu-close--hidden");
+});
+
+cartOpen.addEventListener("click", function () {
+  cartDetail.classList.toggle("header__cart-detail--hidden");
 });
