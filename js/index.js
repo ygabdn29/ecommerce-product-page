8a11a47f09ac628ad1980fbb1f18658b4848de14
Yoga <yogabudiman07@gmail.com>
2023-11-07 19:21:15 +0700

    Desktop and mobile design

--- js/index.js
+++ js/index.js
@@ -27,6 +27,15 @@ const lightboxImages = document.querySelectorAll(
 );
 const lightboxClose = document.querySelector(".product-page__lightbox-close");
 const mobileNextBtn = document.querySelector(".product-page__next");
+const qtyInput = document.querySelector(".product-page__product-qty");
+const numberForm = document.querySelector(".product-page__qty-input");
+const minusBtn = document.querySelector(".product-page__qty-count--min");
+const addBtn = document.querySelector(".product-page__qty-count--add");
+const min = parseInt(qtyInput.getAttribute("min"));
+const qtyForm = document.querySelector(".product-page__form");
+const cartContent = document.querySelector(".header__cart-content");
+const emptyCartText = document.querySelector(".header__text-cart-empty");
+const totalQtyContainer = document.querySelector(".header__cart-total-item");
 
 function changeActiveImages(lightbox = false, e) {
   if (lightbox) {
@@ -102,4 +111,77 @@ lightboxImageGallery.addEventListener("click", function (e) {
 
 mobileNextBtn.addEventListener("click", function () {
   lightbox.classList.add("product-page__lightbox--active");
+});
+
+qtyInput.addEventListener("change", function () {});
+
+numberForm.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("product-page__qty-count")) return;
+  const operator = e.target.getAttribute("data-action");
+  let inputQty = parseInt(qtyInput.value);
+
+  if (operator === "add") {
+    inputQty += 1;
+  }
+
+  if (operator === "min") {
+    inputQty = inputQty <= min ? min : (inputQty -= 1);
+
+    if (inputQty == min) {
+      minusBtn.disabled = true;
+    }
+  }
+
+  qtyInput.value = inputQty;
+});
+
+qtyForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const productQty = Object.fromEntries(new FormData(e.target));
+  let cartItem = `                
+  <div class="header__cart-item">
+    <div class="header__cart-item-container">
+      <div class="header__cart-item-thumbnail">
+        <img
+          src="./images/image-product-1-thumbnail.jpg"
+          alt=""
+          class="header__cart-item-thumbnail-image"
+        />
+      </div>
+
+      <div class="header__cart-item-detail">
+        <p class="header__cart-item-name">
+          Fall Limited Edition Sneakers
+        </p>
+
+        <div class="header__cart-item-price">
+          <span class="header__cart-item-price">$125.00</span>
+          <span class="header__cart-item-quantity">x ${
+            productQty["product-qty"]
+          }</span>
+          <span class="header__cart-item-total-price"
+            >$${125 * parseInt(productQty["product-qty"])}.00</span
+          >
+        </div>
+      </div>
+
+      <div class="header__cart-item-delete">
+        <button class="header__cart-item-delete-btn">
+          <img src="./images/icon-delete.svg" alt="" />
+        </button>
+      </div>
+    </div>
+
+    <button class="header__cart-item-checkout-btn">
+      Checkout
+    </button>
+</div>`;
+
+  let totalQty = `<p class="header__cart-total-item-text">${productQty["product-qty"]}</p>`;
+
+  emptyCartText.classList.add("header__text-cart-empty--hidden");
+  cartContent.insertAdjacentHTML("afterbegin", cartItem);
+
+  totalQtyContainer.classList.remove("header__cart-total-item--hidden");
+  totalQtyContainer.insertAdjacentHTML("afterbegin", totalQty);
 });
